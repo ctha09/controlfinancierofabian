713d49fe7362ce2fd34c7fb9a3199abca0826ef4
CDE <carlostradingfxft@gmail.com>
2026-03-29 19:08:31 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,5 @@
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
-});
-
+self.addEventListener('install', (e) => self.skipWaiting());
 self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(keys.map((key) => caches.delete(key)));
-    })
-  );
+    e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
 });
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request));
-});
+self.addEventListener('fetch', (e) => e.respondWith(fetch(e.request)));
