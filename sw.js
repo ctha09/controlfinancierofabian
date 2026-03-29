a616f3d586fa92454f7037ac775f25870ebf6c97
CDE <carlostradingfxft@gmail.com>
2026-03-29 18:54:05 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,11 @@
+// Cambiamos el nombre a v9 para forzar la actualización
+const CACHE_NAME = 'financeflow_elite_v9';
+
+self.addEventListener('install', (event) => {
+    self.skipWaiting(); // Obliga al service worker nuevo a tomar el control
+});
+
 self.addEventListener('fetch', (event) => {
-    // No guarda caché para que siempre veas tus cambios de hoy
+    // No guarda nada en caché para que siempre veas tu código real
     event.respondWith(fetch(event.request));
 });
