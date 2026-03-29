b3e92f1b3ac815bfe796169fb2559472a5dced0c
CDE <carlostradingfxft@gmail.com>
2026-03-29 18:50:34 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,31 +1,4 @@
-const CACHE_NAME = 'v3_financeflow_cache'; // Versión 3
-const assets = [
-  './',
-  './index.html',
-  './manifest.json'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(assets);
-    })
-  );
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(keys
-        .filter(key => key !== CACHE_NAME)
-        .map(key => caches.delete(key))
-      );
-    })
-  );
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+self.addEventListener('fetch', (event) => {
+    // No guarda caché para que siempre veas tus cambios de hoy
+    event.respondWith(fetch(event.request));
 });
