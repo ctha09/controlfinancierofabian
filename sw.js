f18f594849b03e23743e3bfdabbc0ce9a1855b05
CDE <carlostradingfxft@gmail.com>
2026-03-30 17:28:54 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,7 @@
-self.addEventListener('install', (e) => self.skipWaiting());
-self.addEventListener('activate', (e) => {
-    e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
+const CACHE = 'finance-v7';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./', './index.html', './style.css', './script.js'])));
 });
-self.addEventListener('fetch', (e) => e.respondWith(fetch(e.request)));
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
