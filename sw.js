23dce54c4dc024d60faeda5dc89643645b0c40a7
CDE <carlostradingfxft@gmail.com>
2026-04-04 23:13:28 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,19 @@
-const CACHE = 'finance-v8.0';
-const assets = [
-    './',
-    './index.html',
-    './style.css',
-    './script.js'
-];
+const CACHE = 'finance-v10.0';
+const assets = ['./', './index.html', './style.css', './script.js'];
 
 self.addEventListener('install', e => {
-    self.skipWaiting(); // Fuerza la activación inmediata
-    e.waitUntil(
-        caches.open(CACHE).then(c => c.addAll(assets))
-    );
+    self.skipWaiting();
+    e.waitUntil(caches.open(CACHE).then(c => c.addAll(assets)));
 });
 
 self.addEventListener('activate', e => {
     e.waitUntil(
         caches.keys().then(keys => {
-            return Promise.all(
-                keys.filter(key => key !== CACHE).map(key => caches.delete(key))
-            );
+            return Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)));
         })
     );
 });
 
 self.addEventListener('fetch', e => {
-    e.respondWith(
-        caches.match(e.request).then(r => r || fetch(e.request))
-    );
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
